dcb07dde48849f37ab187a72bf01de055b9f7918
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:53:58 +0530

    Add peri class to calculate perimeter of circle

---
+++ per.java
@@ -0,0 +1,14 @@
+package math;
+
+public class peri {
+
+	public static void main(String[] args) {
+		double r=16;
+//		double pi=3.14;
+		
+		double area=2*Math.PI*r;
+		
+		System.out.println(area);
+		
+	}
+}
