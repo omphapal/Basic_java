a2ed9fb30ad5b62854a57a53407d5d7aef88bbe8
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:54:52 +0530

    Add DemoOperator class for basic arithmetic operations

---
+++ DemoOpertator.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class DemoOpertator {
+
+	public static void main(String[] args) {
+
+		int num1=45,num2=20;
+		System.out.println("Add of num1,num2="+(num1+num2));
+		System.out.println("sub of num1,num2="+(num1-num2));
+		System.out.println("mul of num1,num2="+num1*num2);
+		System.out.println("div of num1,num2="+num1/num2);
+		System.out.println("mod of num1,num2="+num1%num2);
+		
+	}
+
+}
