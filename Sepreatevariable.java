ada1a72245277752860d5b8c8796316589cd84e4
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:52:16 +0530

    Add program to separate and print digits of a number
    This program separates the digits of a number and prints each digit.

---
+++ Sepreatevariable.java
@@ -0,0 +1,28 @@
+package jan_27_2;
+
+public class Sepreatevariable {
+
+	public static void main(String[] args) {
+		
+		int num=23451;
+		
+		System.out.println(num%10);
+		
+		int num1=num/10;
+		System.out.println(num1%10);
+		
+		int num2=num1/10;
+		System.out.println(num2%10);
+		
+		int num3=num2/10;
+		System.out.println(num3%10);
+		
+		int num4=num3/10;
+		System.out.println(num4%10);
+		
+		int num5=num4/10;
+		System.out.println(num5%10);
+
+	}
+
+}
