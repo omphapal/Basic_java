d69c4af5ba87c0b5559e5594dcc1a7b116146fe4
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:58:42 +0530

    Add explict.java for type casting demonstration

---
+++ explict.java
@@ -0,0 +1,14 @@
+package typecasting;
+
+public class explict {
+
+	public static void main(String[] args) {
+		
+		int i=100;
+		long l=i;//no explict type casting requried
+		float f=l;//no explict type casting requried
+		System.out.println("Int value "+i);
+		System.out.println("Long value"+l);
+		System.out.println("Float value"+f);
+	}
+}
