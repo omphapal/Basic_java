3c9dfccb1a68c8ccb95c99e3163ca55a36f2cc85
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:58:06 +0530

    Add Byte class for user input of various types

---
+++ Byte.java
@@ -0,0 +1,63 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Byte {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+//		byte  a;
+//		System.out.println("Enter a byt");
+//		a=sc.nextByte();
+//		System.out.println("byte"+a);
+//		
+//		short s;
+//		System.out.println("Enter a short");
+//		s=sc.nextShort();
+//		System.out.println("short"+s);
+//		
+//		long l;
+//		System.out.println("Enter long");
+//		l=sc.nextLong();
+//		System.out.println("long"+l);
+//		
+//		float f;
+//		System.out.println("Enter float");
+//		f=sc.nextFloat();
+//		System.out.println("float"+f);
+//		
+//		double d;
+//		System.out.println("enter double");
+//		d=sc.nextDouble();
+//		System.out.println("double"+d);
+//		
+//		
+//		boolean bo;
+//		System.out.println("Enter boolean");
+//		bo=sc.nextBoolean();
+//		System.out.println("boolean "+bo);
+//		
+//		String s1;
+//		System.out.println("Enter single setnce");
+//		s1=sc.next();
+//		System.out.println("String"+s1);
+//
+//		
+//		String s2;
+//		System.out.println("Enter a single when to ling");
+//		s2=sc.nextLine();
+//		System.out.println("String2"+s2);
+
+		
+//		int c = 0;
+//		System.out.println("Enter charcter");
+//		c=sc.next().charAt();
+//		System.out.println(c);
+//		
+		char ch;
+		System.out.println("Enter string");
+		ch=sc.next().charAt(0);
+		System.out.println(ch);
+	}
+}
