e782de606db8186955e27a5ac5acbd92f53069fc
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-01-28 08:57:07 +0530

    Implement basic arithmetic operations in Addition class

---
+++ Scanner.java
@@ -0,0 +1,35 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of a ");
+		int a=sc.nextInt();
+		
+		System.out.println("Enter value of b");
+		int b=sc.nextInt();
+		
+		int c=a+b;
+		
+		System.out.println("Addition ="+c);
+		
+		c=a-b;
+		System.out.println("sub ="+c);
+		
+		c=a/b;
+		System.out.println("div ="+c);
+		
+		c=a*b;
+		System.out.println("mul ="+c);
+		
+		 c=a%b;
+		System.out.println("mul ="+c);
+		
+		
+		
+	}
+}
